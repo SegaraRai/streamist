5d0ea3af07932a11e70f9f663dc5b49fc0187d2b
SegaraRai <SegaraRai@users.noreply.github.com>
2021-12-14 05:14:16 +0900

    use iso format

--- packages/server/db/lib/dateTime.ts
+++ packages/server/db/lib/dateTime.ts
@@ -1,14 +1,3 @@
-import type { getPrismaClient } from '@prisma/client/runtime';
-import { client } from './client';
-
-export function dbFormatDateTime(date = new Date()): string | number {
-  const internalClient = client as unknown as InstanceType<
-    ReturnType<typeof getPrismaClient>
-  >;
-  const provider = internalClient._engineConfig.activeProvider;
-  switch (provider) {
-    case 'sqlite':
-      return date.getTime();
-  }
-  throw new Error(`unsupported provider: ${provider}`);
+export function dbFormatDateTime(date = new Date()): string {
+  return date.toISOString();
 }
