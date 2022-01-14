aa63b9a33d1222414b8121296c605216f70db4fe
SegaraRai <SegaraRai@users.noreply.github.com>
2022-01-15 03:36:49 +0900

    feat: add IS_MAINTENANCE flag
    we should find better way to do this

--- packages/frontend/_worker.js
+++ packages/frontend/_worker.js
@@ -1,20 +1,31 @@
+const IS_MAINTENANCE = false;
+
 export default {
   fetch(request, env) {
     const url = new URL(request.url);
     if (url.pathname.startsWith('/api/')) {
-      const newRequest = new Request(
-        `${env.BACKEND_API_ORIGIN}${url.pathname}${url.search}`,
-        request
-      );
-      newRequest.headers.set(
-        'X-Backend-Authorization',
-        `Bearer ${env.BACKEND_API_TOKEN}`
-      );
-      newRequest.headers.set(
-        'X-Backend-CF-Connecting-IP',
-        request.headers.get('cf-connecting-ip')
-      );
-      return fetch();
+      if (IS_MAINTENANCE) {
+        return new Response('down for maintenance', {
+          status: 503,
+          headers: {
+            'Cache-Control': 'no-store',
+          },
+        });
+      } else {
+        const newRequest = new Request(
+          `${env.BACKEND_API_ORIGIN}${url.pathname}${url.search}`,
+          request
+        );
+        newRequest.headers.set(
+          'X-Backend-Authorization',
+          `Bearer ${env.BACKEND_API_TOKEN}`
+        );
+        newRequest.headers.set(
+          'X-Backend-CF-Connecting-IP',
+          request.headers.get('cf-connecting-ip')
+        );
+        return fetch(newRequest);
+      }
     }
     return env.ASSETS.fetch(request);
   },
