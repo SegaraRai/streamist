17dadac77149539fb9f0efcda8f4bf397b3d3aea
SegaraRai <SegaraRai@users.noreply.github.com>
2022-01-15 03:35:14 +0900

    fix: use just 'no-store'

--- packages/shared/config/cacheControl.ts
+++ packages/shared/config/cacheControl.ts
@@ -3,4 +3,4 @@ export const CACHE_CONTROL_PUBLIC_IMMUTABLE =
   'public, immutable, max-age=31536000';
 export const CACHE_CONTROL_PRIVATE_IMMUTABLE =
   'private, immutable, max-age=31536000';
-export const CACHE_CONTROL_NO_STORE = 'no-store, no-cache, must-revalidate';
+export const CACHE_CONTROL_NO_STORE = 'no-store';
