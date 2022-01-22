82a9cb1146ad8a269074380c05c931f6666c5c78
SegaraRai <SegaraRai@users.noreply.github.com>
2022-01-22 15:51:40 +0900

    chore(server): use rollup for production build
    but we may not need rollup for server program

---
+++ packages/server/rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import { defineConfig } from 'rollup';
+import esbuild from 'rollup-plugin-esbuild';
+// import { terser } from 'rollup-plugin-terser';
+import tsPaths from 'rollup-plugin-tsconfig-paths';
+
+const { NODE_ENV = 'production' } = process.env;
+if (NODE_ENV !== 'production' && NODE_ENV !== 'development') {
+  throw new Error(`NODE_ENV must be 'production' or 'development'`);
+}
+
+export default defineConfig({
+  input: 'entrypoints/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    inlineDynamicImports: true,
+  },
+  plugins: [
+    tsPaths({
+      // load other project's tsconfig.json
+      // this is required to resolve child project dependencies (such as `$shared`) imported from project dependencies (such as $shared-server)
+      tsConfigPath: [
+        'tsconfig.json',
+        '../shared/tsconfig.json',
+        '../shared-server/tsconfig.json',
+        '../transcoder/tsconfig.json',
+      ],
+    }),
+    json({
+      preferConst: true,
+    }),
+    nodeResolve({
+      extensions: ['.js', '.mjs', '.ts', '.json', '.node'],
+      exportConditions: ['node'],
+      preferBuiltins: true,
+    }),
+    esbuild({
+      define: {
+        'process.env.NODE_ENV': `"${NODE_ENV}"`,
+      },
+      minify: false,
+    }),
+    commonjs(),
+    // terser(),
+  ],
+});
