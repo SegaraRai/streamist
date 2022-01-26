8e090cc325a9f8de89d6008279f2f563c2c3826e
SegaraRai <SegaraRai@users.noreply.github.com>
2022-01-26 23:12:08 +0900

    fix(transcoder/gcr): keep connection during transcode

--- packages/transcoder/src/indexGCR.ts
+++ packages/transcoder/src/indexGCR.ts
@@ -11,8 +11,8 @@ import type { TranscoderRequest } from './types';
 
 const server = createServer();
 
-server.on('request', (req, res) => {
-  (async () => {
+server.on('request', (req, res): void => {
+  (async (): Promise<void> => {
     if (req.method === 'POST' && req.url === '/api/transcode') {
       if (!/^application\/json;?/.test(req.headers['content-type'] || '')) {
         res.statusCode = 400;
@@ -26,14 +26,10 @@ server.on('request', (req, res) => {
       ) as TranscoderRequest;
       logger.info(transcoderRequest);
 
-      transcode(transcoderRequest)
-        .then((transcoderResponse) => {
-          logger.info(transcoderResponse);
-          return sendCallback(transcoderRequest, transcoderResponse);
-        })
-        .catch((error) => {
-          logger.error(error);
-        });
+      const transcoderResponse = await transcode(transcoderRequest);
+      logger.info(transcoderResponse);
+
+      await sendCallback(transcoderRequest, transcoderResponse);
 
       res.statusCode = 204;
       res.end();
