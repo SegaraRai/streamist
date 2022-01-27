2c77ab584fd7d627a754d5d7a482ef71d9a68cd5
SegaraRai <SegaraRai@users.noreply.github.com>
2022-01-28 07:24:35 +0900

    fix: correct symlink path [skip ci]

--- packages/transcoder/createLayer.sh
+++ packages/transcoder/createLayer.sh
@@ -5,7 +5,7 @@ cp ffmpeg /tmp/transcoder-lambda-layer/bin/
 cp magick /tmp/transcoder-lambda-layer/bin/
 cp mkclean /tmp/transcoder-lambda-layer/bin/
 chmod +x /tmp/transcoder-lambda-layer/bin/*
-ln -s /tmp/transcoder-lambda-layer/bin/ffmpeg /tmp/transcoder-lambda-layer/bin/ffprobe
+ln -s ./ffmpeg /tmp/transcoder-lambda-layer/bin/ffprobe
 pushd /tmp/transcoder-lambda-layer
 zip -ry9 /tmp/transcoder-lambda-layer.zip *
 popd
