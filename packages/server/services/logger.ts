92ffa6df8483d507cf9fde679f6c55d730ab6f2f
SegaraRai <SegaraRai@users.noreply.github.com>
2022-01-27 17:27:09 +0900

    chore: disable pino-pretty for non-dev

--- packages/server/services/logger.ts
+++ packages/server/services/logger.ts
@@ -1,12 +1,16 @@
 import { pino } from 'pino';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:HH:MM:ss.l o',
-      ignore: 'hostname',
-    },
-  },
-});
+export const logger = pino(
+  process.env.NODE_ENV === 'development'
+    ? {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:HH:MM:ss.l o',
+            ignore: 'hostname',
+          },
+        },
+      }
+    : {}
+);
