1952a26143078cbb639fa4c10326037d9629a1ee
SegaraRai <SegaraRai@users.noreply.github.com>
2022-02-04 02:28:00 +0900

    feat: skip by swipe in mobile playback control, close #121

--- packages/frontend/src/components/SMobilePlaybackControl.vue
+++ packages/frontend/src/components/SMobilePlaybackControl.vue
@@ -1,14 +1,41 @@
 <script lang="ts">
+import { SwipeDirection } from '@vueuse/core';
 import { useCurrentTrackInfo } from '~/composables';
+import { SWIPE_DISTANCE_THRESHOLD } from '~/config';
 import { usePlaybackStore } from '~/stores/playback';
 
 export default defineComponent({
   setup() {
     const playbackStore = usePlaybackStore();
-
     const { value: currentTrackInfo } = useCurrentTrackInfo();
 
+    const container$$q = ref<HTMLElement | null | undefined>();
+    const containerWidth = computed(() => container$$q.value?.offsetWidth);
+    const { lengthX } = useSwipe(container$$q, {
+      passive: true,
+      onSwipeEnd(_e: TouchEvent, direction: SwipeDirection) {
+        const trigger =
+          containerWidth.value &&
+          Math.abs(lengthX.value) / containerWidth.value >=
+            SWIPE_DISTANCE_THRESHOLD;
+        if (!trigger) {
+          return;
+        }
+
+        switch (direction) {
+          case SwipeDirection.LEFT:
+            playbackStore.skipNext$$q();
+            break;
+
+          case SwipeDirection.RIGHT:
+            playbackStore.goPrevious$$q();
+            break;
+        }
+      },
+    });
+
     return {
+      container$$q,
       currentTrackInfo$$q: currentTrackInfo,
       playing$$q: playbackStore.playing$$q,
       progress$$q: computed(() =>
@@ -52,7 +79,10 @@ export default defineComponent({
     @mousedown="preventXButton$$q"
     @mouseup="preventXButton$$q($event), onMouseUp$$q($event)"
   >
-    <div class="w-full flex-1 flex flex-row px-4 items-center">
+    <div
+      ref="container$$q"
+      class="w-full flex-1 flex flex-row px-4 items-center"
+    >
       <RouterLink
         class="flex-1 flex items-center overflow-hidden"
         to="/playing"
