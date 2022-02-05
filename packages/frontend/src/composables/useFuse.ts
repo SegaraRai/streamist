9d9bdace032755545c2122785cfabd6886213cdb
SegaraRai <SegaraRai@users.noreply.github.com>
2022-02-06 02:46:09 +0900

    perf: lazy load fuse.js

--- packages/frontend/src/composables/useFuse.ts
+++ packages/frontend/src/composables/useFuse.ts
@@ -1,11 +1,16 @@
-import Fuse from 'fuse.js';
+import type Fuse from 'fuse.js';
 import type { Ref } from 'vue';
 
 export function useFuse<T>(
   items: Readonly<Ref<readonly T[] | undefined>>,
   options: Fuse.IFuseOptions<T>
-): Readonly<Ref<Fuse<T>>> {
-  return computed(() => {
-    return new Fuse(items.value || [], options);
+): Readonly<Ref<Fuse<T> | undefined>> {
+  const FusePromise = import('fuse.js');
+  const FuseRef = ref<typeof Fuse | undefined>();
+  FusePromise.then((f): void => {
+    FuseRef.value = f.default;
   });
+  return computed(() =>
+    FuseRef.value ? new FuseRef.value(items.value || [], options) : undefined
+  );
 }
