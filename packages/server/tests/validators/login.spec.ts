9d88bcf522d4ea21b0ada74ab5003c37f2bad3f5
SegaraRai <SegaraRai@users.noreply.github.com>
2022-02-07 19:52:39 +0900

    fix: fix tests

---
+++ packages/server/tests/validators/login.spec.ts
@@ -0,0 +1,62 @@
+import '$/services/initOS';
+
+import { describe, expect, it } from 'vitest';
+import { VAuthBodyPassword } from '$/validators';
+import { validate } from './utils';
+
+describe('VAuthBodyWrapper', () => {
+  it('should accept correct payloads', async () => {
+    await expect(
+      validate(VAuthBodyPassword, {
+        grant_type: 'password',
+        username: 'test_id',
+        password: 'test_password',
+      })
+    ).resolves.toBeUndefined();
+  });
+
+  it('should reject malformed payloads', async () => {
+    // invalid grant_type
+    await expect(
+      validate(VAuthBodyPassword, {
+        grant_type: 'foo' as any,
+        username: 'test_id',
+        password: 'test_password',
+      })
+    ).rejects.toMatchSnapshot();
+
+    // missing grant_type
+    await expect(
+      validate(VAuthBodyPassword, {
+        username: 'test_id',
+        password: 'test_password',
+      } as any)
+    ).rejects.toMatchSnapshot();
+
+    // invalid username
+    await expect(
+      validate(VAuthBodyPassword, {
+        grant_type: 'password',
+        username: { toString: 'test_id' },
+        password: 'test_password',
+      } as any)
+    ).rejects.toMatchSnapshot();
+
+    // invalid password
+    await expect(
+      validate(VAuthBodyPassword, {
+        grant_type: 'password',
+        username: 'test_id',
+        password: 123 as any,
+      } as any)
+    ).rejects.toMatchSnapshot();
+
+    // missing password
+    await expect(
+      validate(VAuthBodyPassword, {
+        grant_type: 'password',
+        username: 'test_id',
+      } as any)
+    ).rejects.toMatchSnapshot();
+  });
+});
