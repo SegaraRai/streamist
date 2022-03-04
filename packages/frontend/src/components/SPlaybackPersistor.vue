7234ac4e23b43b59a1287a6860dd478f6601d371
SegaraRai <SegaraRai@users.noreply.github.com>
2022-03-05 01:50:33 +0900

    chore(SPlaybackPersistor): persist useAccurateTime and useWS explicitly

--- packages/frontend/src/components/SPlaybackPersistor.vue
+++ packages/frontend/src/components/SPlaybackPersistor.vue
@@ -1,9 +1,13 @@
 <script lang="ts">
+import { useAccurateTime } from '~/composables/useAccurateTime';
+import { useWS } from '~/composables/useWS';
 import { usePlaybackStore } from '~/stores/playback';
 
 export default defineComponent({
   setup() {
     usePlaybackStore();
+    useWS();
+    useAccurateTime();
 
     return {};
   },
