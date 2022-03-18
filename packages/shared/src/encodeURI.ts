023e758fd7a5981a111c88291f467029ef9ba21c
SegaraRai <SegaraRai@users.noreply.github.com>
2022-03-18 18:29:46 +0900

    perf: use predefined map for URI encoding

--- packages/shared/src/encodeURI.ts
+++ packages/shared/src/encodeURI.ts
@@ -7,14 +7,32 @@ RFC 3986            |`` A-Z a-z 0-9                     - .               _     
 RFC 8187 (RFC 5987) |`` A-Z a-z 0-9 ! # $ &         +   - .             ^ _ ` | ~ ``
 */
 
+const escapeMap: Readonly<Record<string, string>> = {
+  '!': '%21',
+  "'": '%27',
+  '(': '%28',
+  ')': '%29',
+  '*': '%2A',
+};
+
+const unescapeMap: Readonly<Record<string, string>> = {
+  '%23': '#',
+  '%24': '$',
+  '%26': '&',
+  '%2B': '+',
+  '%2b': '+',
+  '%5E': '^',
+  '%5e': '^',
+  '%60': '`',
+  '%7C': '|',
+  '%7c': '|',
+};
+
 export function encodeRFC3986URIComponent(str: string): string {
   return (
     encodeURIComponent(str)
       // escape ! ' ( ) *
-      .replace(
-        /[!'()*]/g,
-        (char) => '%' + char.charCodeAt(0).toString(16).toUpperCase()
-      )
+      .replace(/[!'()*]/g, (c) => escapeMap[c])
   );
 }
 
@@ -22,13 +40,8 @@ export function encodeRFC8187ValueChars(str: string): string {
   return (
     encodeURIComponent(str)
       // escape ' ( ) *
-      .replace(
-        /['()*]/g,
-        (char) => '%' + char.charCodeAt(0).toString(16).toUpperCase()
-      )
+      .replace(/['()*]/g, (c) => escapeMap[c])
       // unescape # $ & + ^ ` |
-      .replace(/%(2[346B]|5E|60|7C)/gi, (_str, hex) =>
-        String.fromCharCode(parseInt(hex, 16))
-      )
+      .replace(/%(?:2[346B]|5E|60|7C)/gi, (s) => unescapeMap[s])
   );
 }
