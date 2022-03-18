988b6f090da855db522a66cea49c15ee433f078c
SegaraRai <SegaraRai@users.noreply.github.com>
2022-03-18 17:37:00 +0900

    feat: add test_dev commands

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-wt nt cmd /k pnpm run test:server -- -- -u ; ^
+wt nt cmd /k pnpm run test_dev:server -- ; ^
 sp -V cmd /k pnpm run typecheck:server -- -- -w ; ^
 mf left ; ^
 sp -H cmd /k pnpm run typecheck:cdn -- -- -w ; ^
